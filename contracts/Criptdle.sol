6a164e3b81d3f7fd7e920bf9e5859c5e01343592
hairo0o <102835212+hairo0o@users.noreply.github.com>
2022-04-18 21:04:07 -0300

    Mejoras!

--- contracts/Criptdle.sol
+++ contracts/Criptdle.sol
@@ -4,18 +4,38 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Criptdle {
+
+    address public owner;
     
-    string[] words;                                   
+    string[] wordsText;     
+
+    struct word {
+        string text;
+        bool used;
+    } 
+
+    mapping(string=>word)public words;   
+     modifier Owner(address _address) {
+        require(_address == owner, "No tenes permiso para ejecutar esta operacion.");
+        _;
+    }                
 
     constructor() {
+        owner = msg.sender;
+        console.log("La address owner del contrato es: ", owner);
         console.log("deployando el contrato");
     }
 
     function createWord(string memory _word) public {
-        words.push(_word);
+        words[_word] = word(_word, false);
+        wordsText.push(_word);
     }
 
     function readWords() public view returns(string[] memory) {
-        return words;
+        return wordsText;
+    }
+
+    function useWord(string memory _word) public {
+        words[_word] = word(_word, true);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,15 +6,22 @@ async function main() {
   await criptdle.deployed();
 
   console.log("Criptdle deployed to:", criptdle.address);
-  
+  /*
   let tx = await criptdle.createWord("solow");
 
   tx = await criptdle.createWord("fork");
 
   tx = await criptdle.createWord("blockchain");
 
-  let words = await criptdle.readWords();
-  console.log(words);
+  tx = await criptdle.readWords();
+  console.log(tx);
+
+  tx = await criptdle.useWord("wagmi")
+
+  tx = await criptdle.words("wagmi");
+  
+  console.log(tx);
+  */
 }
 
 main()
